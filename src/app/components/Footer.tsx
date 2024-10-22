b6cfbe7d485fed0726b9e2bebfa8ec2d35031880
MuhammadMomin095 <160387789+MuhammadMomin095@users.noreply.github.com>
2024-10-23 00:46:38 +0500

    Portfolio

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,28 @@
+import { FaLinkedin, FaGithub, FaFacebook, FaInstagram } from 'react-icons/fa';
+import Link from 'next/link';
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-950 text-white py-4">
+      <div className="container mx-auto text-center">
+        <p className="mb-2">© {new Date().getFullYear()} Muhammad Momin. All rights reserved.</p>
+        <div className="flex justify-center space-x-4">
+          <Link href="https://www.linkedin.com/in/muhammad-momin-62069b2b9/" target="_blank">
+            <FaLinkedin className="hover:text-emerald-500 cursor-pointer" size={24} />
+          </Link>
+          <Link href="https://github.com/MuhammadMomin095" target="_blank">
+            <FaGithub className="hover:text-emerald-500 cursor-pointer" size={24} />
+          </Link>
+          <Link href="https://www.instagram.com/mominshykh/?hl=en" target="_blank">
+            <FaInstagram className="hover:text-emerald-500 cursor-pointer" size={24} />
+          </Link>
+          <Link href="https://www.facebook.com/mominshykh.xx" target="_blank">
+            <FaFacebook className="hover:text-emerald-500 cursor-pointer" size={24} />
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
